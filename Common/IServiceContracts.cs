b059098083f01b2a4e0e9126f53032431d8f15c2
DusanKne <101563317+DusanKne@users.noreply.github.com>
2023-02-03 18:31:44 +0100

    Add files via upload

---
+++ Common/IServiceContracts.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IServiceContracts
+    {
+        [OperationContract]
+        void StartTimer();
+        [OperationContract]
+        void StopTimer();
+        [OperationContract]
+        void CancelTimer();
+        [OperationContract]
+        void SetTimer(string miliseconds);
+        [OperationContract]
+        int ReadTimer();
+    }
+}
