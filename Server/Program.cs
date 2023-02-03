996413502c496fe4a622413cafaa7404700cc266
DusanKne <101563317+DusanKne@users.noreply.github.com>
2023-02-03 18:33:10 +0100

    Add files via upload

---
+++ Server/Program.cs
@@ -0,0 +1,52 @@
+﻿using Common;
+using SecurityManager;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Policy;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/TimerService";
+
+            binding.Security.Mode = SecurityMode.Transport;
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            ServiceHost host = new ServiceHost(typeof(TimerService));
+            host.AddServiceEndpoint(typeof(IServiceContracts), binding, address);
+
+            host.Authorization.PrincipalPermissionMode = PrincipalPermissionMode.Custom;
+            List<IAuthorizationPolicy> policies = new List<IAuthorizationPolicy>();
+            policies.Add(new CustomAuthorizationPolicy());
+            host.Authorization.ExternalAuthorizationPolicies = policies.AsReadOnly();
+
+            ServiceSecurityAuditBehavior newAudit = new ServiceSecurityAuditBehavior();
+            newAudit.AuditLogLocation = AuditLogLocation.Application;
+            newAudit.ServiceAuthorizationAuditLevel = AuditLevel.SuccessOrFailure;
+
+            host.Description.Behaviors.Remove<ServiceSecurityAuditBehavior>();
+            host.Description.Behaviors.Add(newAudit);
+
+            try
+            {
+                TimerService.InitiateTimer();
+                host.Open();
+                Console.WriteLine("Stopwatch service started successfully at: " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+        }
+    }
+}
---
+++ Server/TimerService.cs
@@ -0,0 +1,150 @@
+﻿using Common;
+using SecurityManager;
+using System.Timers;
+using System;
+using System.Threading;
+using System.ServiceModel;
+using System.Security.Permissions;
+
+namespace Server
+{
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    internal class TimerService : IServiceContracts
+    {
+        private static System.Timers.Timer _timer = new System.Timers.Timer();
+        private static int milisecondsLeft = 0;
+        [PrincipalPermission(SecurityAction.Demand, Role = "Change")]
+        public void CancelTimer()
+        {
+            CustomPrincipal principal = Thread.CurrentPrincipal as CustomPrincipal;
+            string username = Formatter.ParseName(principal.Identity.Name);
+
+            if (milisecondsLeft > 0)
+            {
+                _timer.Stop();
+                milisecondsLeft = 0;
+                try
+                {
+                    Audit.ActionSuccesful(username, "CancelTimer");
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+            else
+            {
+                try
+                {
+                    Audit.ActionFailed(username, "CancelTimer");
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+                throw new FaultException("Timer is already at 0.");
+
+            }
+
+        }
+
+        [PrincipalPermission(SecurityAction.Demand, Role = "See")]
+        public int ReadTimer()
+        {
+            return milisecondsLeft;
+
+        }
+
+        [PrincipalPermission(SecurityAction.Demand, Role = "Change")]
+        public void SetTimer(string miliseconds)
+        {
+            CustomPrincipal principal = Thread.CurrentPrincipal as CustomPrincipal;
+            string username = Formatter.ParseName(principal.Identity.Name);
+            int input;
+            bool correctInput = Int32.TryParse(EncryptionManager.DecryptMessage(miliseconds), out input);
+            if (correctInput)
+            {
+                milisecondsLeft = input;
+                try
+                {
+                    Audit.ActionSuccesful(username, "SetTimerTime");
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            }
+            else
+            {
+                try
+                {
+                    Audit.ActionFailed(username, "SetTimerTime");
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+      
+                throw new FaultException("Wrong input, please try again.");
+            }
+        }
+
+        [PrincipalPermission(SecurityAction.Demand, Role = "StartStop")]
+        public void StartTimer()
+        {
+            CustomPrincipal principal = Thread.CurrentPrincipal as CustomPrincipal;
+            string username = Formatter.ParseName(principal.Identity.Name);
+            if (milisecondsLeft > 0)
+            {
+                _timer.Enabled = true;
+                _timer.Start();
+                try
+                {
+                    Audit.ActionSuccesful(username, "StartTimer");
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+               
+            }
+            else
+            {
+                try
+                {
+                    Audit.ActionFailed(username, "StartTimer");
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+                throw new FaultException("Timer has elapsed, input a time to start it again.");
+            }
+        }
+
+        [PrincipalPermission(SecurityAction.Demand, Role = "StartStop")]
+        public void StopTimer()
+        {
+            CustomPrincipal principal = Thread.CurrentPrincipal as CustomPrincipal;
+            string username = Formatter.ParseName(principal.Identity.Name);
+            if (milisecondsLeft > 0)
+            {
+                _timer.Stop();
+                try
+                {
+                    Audit.ActionSuccesful(username, "StopTimer");
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+            else
+            {
+                try
+                {
+                    Audit.ActionFailed(username, "StopTimer");
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+                throw new FaultException("Timer has elapsed, cannot be stopped.");
+            }
+        }
+
+
+        private static void HandleEvent(object sender, EventArgs e)
+        {
+            milisecondsLeft -= 100;
+            if(milisecondsLeft == 0)
+            {
+                _timer.Stop();
+            }
+        }
+        
+        public static void InitiateTimer()
+        {
+            _timer.Enabled = false;
+            _timer.Elapsed += new ElapsedEventHandler(HandleEvent);
+            _timer.Interval = 100;
+        }
+    }
+}
